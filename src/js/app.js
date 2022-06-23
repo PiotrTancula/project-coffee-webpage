64182eab381c2ade0c6e50798bf14f1b3fbcbc28
Piotr Tańcula <tanculapiotr@gmail.com>
2022-06-23 12:20:04 +0200

    home page is present && you can switch the pages

---
+++ src/js/app.js
@@ -0,0 +1,65 @@
+import { classNames, select } from './settings.js';
+
+const app = {
+
+  activatePage: function (pageId) {
+
+    const thisApp = this;
+    thisApp.sections = document.querySelectorAll(select.sections);
+    console.log(thisApp.sections[2].classList);
+    console.log(thisApp.sections);
+    for( let page of thisApp.pages){
+      if(page.id == pageId){
+        page.classList.add(classNames.pagesActive);
+      }else{
+        page.classList.remove(classNames.pagesActive);
+      }
+    }
+
+    for(let section of thisApp.sections){
+      if(section.classList == pageId){
+        section.classList.add(classNames.pagesActive);
+      }else{
+        section.classList.remove(classNames.pagesActive);
+      }
+    }
+
+    console.log(thisApp.pages);
+
+  },
+
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.mainMenuNavLinksSelector).children;
+
+    thisApp.activatePage(thisApp.pages[0].id);
+    console.log(thisApp.pages[0].classList);
+    console.log(thisApp.pages[1].classList);
+    console.log(thisApp.pages[2].classList);
+
+    for (let link of thisApp.pages){
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#','');
+        thisApp.activatePage(id);
+
+      });
+    }
+    console.log(thisApp.pages[0].classList);
+    console.log(thisApp.pages[1].classList);
+    console.log(thisApp.pages[2].classList);
+  },
+
+
+
+  init: function () {
+    const thisApp = this;
+    thisApp.initPages();
+
+  }
+
+};
+
+app.init();
---
+++ src/js/settings.js
@@ -0,0 +1,12 @@
+export const select = {
+  mainMenuNavLinksSelector: '.menu nav',
+  sections : 'section',
+};
+
+export const classNames = {
+  pagesActive: 'active',
+};
+
+
+
+
